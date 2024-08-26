25e3cf3a5c68cf11067f4a3b944214c1ed472e2b
Minwoo Im <minwoo.im@samsung.com>
2024-08-26 14:18:44 +0900

    examples: Add unvme-reset test case
    Signed-off-by: Minwoo Im <minwoo.im@samsung.com>

---
+++ examples/unvme-reset.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Scenario
+#  - Configure admin sq/cq with enabling controller
+#  - Reset the controller by setting CC.EN=0 and wait for CSTS.RDY=0
+
+if [[ $# -ne 1 ]]; then
+	echo "Usage: $0 <bdf>"
+	exit 1
+fi
+bdf="$1"
+
+unvme start
+unvme add $bdf
+unvme enable $bdf
+unvme reset $bdf
+unvme stop
