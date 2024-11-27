SamsungDS/unvme-cli

commit eee272b3521eb0c8550a290688d76b80634ccd76

unvmed: disable irq in admin cq by default
Even if admin cq vector is passed with 0 to libvfn, do not prepare the
irq handler in libunvmed since libvfn admin command helpers (e.g., queue
creation helpers) do not consider interrupt handling yet.

libunvmed handles irq within the cq reaper thread (rcq) in another
thread context and reap the cq entries and copy them to the rcq to be
fetched by the application.  If libvfn tries to reap cq entries after
reaper thread fetches the target cq entries, libvfn might wait in admin
cq infinitely.  To avoid conflicts between the two, this patch disabled
interrupt initialization in the admin cq by default.

Signed-off-by: Minwoo Im <minwoo.im@samsung.com>
Reviewed-by: Jonghwi Jeong <jongh2.jeong@samsung.com>

lib/libunvmed.c
@@ -1111,21 +1111,14 @@ int unvmed_create_adminq(struct unvme *u)
 	struct unvme_sq *usq;
 	struct unvme_cq *ucq;
 	const uint16_t qid = 0;
-	const uint16_t iv = 0;
 
 	if (unvmed_get_sq(u, 0) || unvmed_get_cq(u, 0)) {
 		errno = EEXIST;
 		goto out;
 	}
 
-	/*
-	 * XXX: Non-intr mode currently not supported
-	 */
-	if (unvmed_init_irq(u, iv))
-		goto out;
-
 	if (nvme_configure_adminq(&u->ctrl, qid))
-		goto free_irq;
+		goto out;
 
 	ucq = unvmed_init_ucq(u, qid);
 	if (!ucq)
@@ -1148,8 +1141,6 @@ free_ucq:
 free_sqcq:
 	nvme_discard_sq(&u->ctrl, &u->ctrl.sq[qid]);
 	nvme_discard_cq(&u->ctrl, &u->ctrl.cq[qid]);
-free_irq:
-	unvmed_free_irq(u, iv);
 out:
 	return -1;
 }

lib/libunvmed.h
@@ -82,7 +82,7 @@ static inline void unvmed_cq_exit(struct unvme_cq *ucq)
 
 static inline bool unvmed_cq_irq_enabled(struct unvme_cq *ucq)
 {
-	return ucq->q->vector >= 0;
+	return unvmed_cq_id(ucq) && ucq->q->vector >= 0;
 }
 
 /*

commit fd96cc5e542fbf957f47b8cc5043f80f64daed38

libunvmed: fix unexpected close() for fd 0
Admin completion queue does not support interrupt event handling in
libunvmed currently.  In the meantime, libvfn initializes admin
completion queue with vector value 0 regardless to the interrupt
support.

When deleting a completion queue instance from libunvmed, we've been
checking 'vector >= 0' to figure out the given completion queue enabled
interrupt handling.  Admin completion queue which actually does not have
interrupt handling at all was passed in this check and go to
unvmed_free_irq() even if it has never initialized irq.  It caused file
descriptor 0 (initial value of r->efds[0]) closed so that all the file
descriptors are messed up.

Check the interrupt support by the provided helper API
unvmed_cq_irq_enabled().

Fixes: eee272b3521e ("unvmed: disable irq in admin cq by default")
Signed-off-by: Minwoo Im <minwoo.im@samsung.com>

lib/libunvmed.c
@@ -1403,11 +1403,12 @@ static void __unvmed_delete_cq(struct unvme *u, struct unvme_cq *ucq)
 {
 	struct nvme_cq *cq = ucq->q;
 	int vector = unvmed_cq_iv(ucq);
+	bool irq = unvmed_cq_irq_enabled(ucq);
 
 	unvmed_cq_put(u, ucq);
 	nvme_discard_cq(&u->ctrl, cq);
 
-	if (vector >= 0)
+	if (irq)
 		unvmed_free_irq(u, vector);
 }
 
@@ -1419,13 +1420,15 @@ static void __unvmed_delete_cq_all(struct unvme *u)
 	pthread_rwlock_wrlock(&u->cq_list_lock);
 	list_for_each_safe(&u->cq_list, ucq, next, list) {
 		int vector = unvmed_cq_iv(ucq);
+		bool irq = unvmed_cq_irq_enabled(__to_cq(ucq));
+
 		cq = ucq->q;
 
 		unvmed_log_info("Deleting ucq (qid=%d)", unvmed_cq_id(ucq));
 		__unvmed_cq_put(u, __to_cq(ucq));
 		nvme_discard_cq(&u->ctrl, cq);
 
-		if (vector >= 0)
+		if (irq)
 			unvmed_free_irq(u, vector);
 	}
 	pthread_rwlock_unlock(&u->cq_list_lock);