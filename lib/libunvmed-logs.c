7c7ec5241befd766756b915dbdc5fe72fa59312a
Minwoo Im <minwoo.im@samsung.com>
2024-12-23 17:31:44 +0900

    libunvmed/logs: fix endian conversion
    @cdw* series should be converted to the cpu's endianness.
    
    Signed-off-by: Minwoo Im <minwoo.im@samsung.com>
    Reviewed-by: Jonghwi Jeong <jongh2.jeong@samsung.com>

--- lib/libunvmed-logs.c
+++ lib/libunvmed-logs.c
@@ -69,11 +69,12 @@ static const char *unvmed_log_admin_identify(union nvme_cmd *sqe)
 
 static const char *unvmed_log_read_write(union nvme_cmd *sqe)
 {
-	uint64_t slba = ((uint64_t) sqe->cdw11 << 32) | sqe->cdw10;
+	uint64_t slba = ((uint64_t) le32_to_cpu(sqe->cdw11) << 32) |
+		le32_to_cpu(sqe->cdw10);
 	uint16_t length = le32_to_cpu(sqe->cdw12) & 0xFF;
 	uint16_t control = (le32_to_cpu(sqe->cdw12) >> 16) & 0xFFFF;
-	uint32_t dsmgmt = sqe->cdw13;
-	uint32_t reftag = sqe->cdw14;
+	uint32_t dsmgmt = le32_to_cpu(sqe->cdw13);
+	uint32_t reftag = le32_to_cpu(sqe->cdw14);
 
 	if (sqe->opcode == 1)
 		snprintf(__buf, LOG_MAX_LEN, "write "
@@ -92,9 +93,10 @@ static const char *unvmed_log_common(union nvme_cmd *sqe)
 	snprintf(__buf, LOG_MAX_LEN, "opcode=0x%x, cdw2=0x%x, cdw3=0x%x, "
 		 "cdw10=0x%x, cdw11=0x%x, cdw12=0x%x, "
 		 "cdw13=0x%x, cdw14=0x%x, cdw15=0x%x",
-		 sqe->opcode, sqe->cdw2, sqe->cdw3,
-		 sqe->cdw10, sqe->cdw11, sqe->cdw12,
-		 sqe->cdw13, sqe->cdw14, sqe->cdw15);
+		 sqe->opcode, le32_to_cpu(sqe->cdw2), le32_to_cpu(sqe->cdw3),
+		 le32_to_cpu(sqe->cdw10), le32_to_cpu(sqe->cdw11),
+		 le32_to_cpu(sqe->cdw12), le32_to_cpu(sqe->cdw13),
+		 le32_to_cpu(sqe->cdw14), le32_to_cpu(sqe->cdw15));
 
 	return __buf;
 }
@@ -140,14 +142,14 @@ void unvmed_log_cmd_post(const char *bdf, uint32_t sqid, union nvme_cmd *sqe)
 	switch(psdt) {
 		case 0:
 			psdt_type = "prp";
-			dptr0 = sqe->dptr.prp1;
-			dptr1 = sqe->dptr.prp2;
+			dptr0 = le64_to_cpu(sqe->dptr.prp1);
+			dptr1 = le64_to_cpu(sqe->dptr.prp2);
 			break;
 		case 1:
 		case 2:
 			psdt_type = "sgl";
-			dptr0 = sqe->dptr.sgl.addr;
-			dptr1 = sqe->dptr.sgl.len;
+			dptr0 = le64_to_cpu(sqe->dptr.sgl.addr);
+			dptr1 = le32_to_cpu(sqe->dptr.sgl.len);
 			break;
 		default:
 			psdt_type = "reserved";
@@ -158,20 +160,21 @@ void unvmed_log_cmd_post(const char *bdf, uint32_t sqid, union nvme_cmd *sqe)
 	unvmed_log_nvme("unvmed_cmd_post: %s: sqe (qid=%d, cid=%d, nsid=%d, "
 		 "fuse=0x%x, psdt=%d(%s), mptr=0x%lx, "
 		 "dptr0=0x%lx, dptr1=0x%lx, cmd=(%s))",
-		 bdf, sqid, sqe->cid, sqe->nsid,
-		 sqe->flags & 0x3, psdt, psdt_type, sqe->mptr,
+		 bdf, sqid, sqe->cid, le32_to_cpu(sqe->nsid),
+		 sqe->flags & 0x3, psdt, psdt_type, le64_to_cpu(sqe->mptr),
 		 dptr0, dptr1, str);
 }
 
 void unvmed_log_cmd_cmpl(const char *bdf, struct nvme_cqe *cqe)
 {
-	uint16_t sfp = cqe->sfp;
+	uint16_t sfp = le16_to_cpu(cqe->sfp);
 
 	unvmed_log_nvme("unvmed_cmd_cmpl: %s: cqe (qid=%d, cid=%d, "
 		 "dw0=0x%x, dw1=0x%x, head=%d, phase=%d, sct=0x%x, sc=0x%x, "
 		 "crd=0x%x, more=%d, dnr=%d)",
 		 bdf, cqe->sqid, cqe->cid,
-		 cqe->dw0, cqe->dw1, cqe->sqhd, sfp & 0x1,
+		 le32_to_cpu(cqe->dw0), le32_to_cpu(cqe->dw1),
+		 le16_to_cpu(cqe->sqhd), sfp & 0x1,
 		 (sfp >> 9) & 0x7, (sfp >> 1) & 0xFF,
 		 (sfp >> 12) & 0xFF, (sfp >> 14) & 0x1, (sfp >> 15) & 0x1);
 }
