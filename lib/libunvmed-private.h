6acdc7e0acbad2efca239ce5ed5ad8a77bf5d9c4
Minwoo Im <minwoo.im@samsung.com>
2025-03-01 02:11:38 +0900

    lib/unvmed: move private struct definitions to header
    Move private structures to libunvmed-private.h.
    
    Signed-off-by: Minwoo Im <minwoo.im@samsung.com>
    Reviewed-by: Jonghwi Jeong <jongh2.jeong@samsung.com>
    Reviewed-by: Haeun Kim <hanee.kim@samsung.com>

--- lib/libunvmed-private.h
+++ lib/libunvmed-private.h
@@ -3,6 +3,9 @@
 #ifndef LIBUNVMED_PRIVATE_H
 #define LIBUNVMED_PRIVATE_H
 
+struct unvme_ctx;
+struct unvme_cq_reaper;
+
 enum unvme_state {
 	UNVME_DISABLED	= 0,
 	UNVME_ENABLED,
@@ -46,6 +49,90 @@ struct unvme {
 	struct nvme_id_ctrl *id_ctrl;
 
 	struct list_node list;
+};
+
+struct __unvme_ns {
+	unvme_declare_ns();
+	struct list_node list;
+};
+
+/*
+ * SQ and CQ instances should not be freed in the runtime even controller reset
+ * happens.  Instances should be remained as is, which means we should reuse
+ * the instance when we re-create a queue with the same qid.  This is because
+ * application (e.g., fio) may keep trying to issue I/O commands with no-aware
+ * of reset behavior.
+ */
+struct __unvme_sq {
+	unvme_declare_sq();
+	struct list_node list;
+};
+#define __to_sq(usq)		((struct unvme_sq *)(usq))
+
+struct __unvme_cq {
+	unvme_declare_cq();
+	struct list_node list;
+};
+#define __to_cq(ucq)		((struct unvme_cq *)(ucq))
+
+/*
+ * Driver context
+ */
+enum unvme_ctx_type {
+	UNVME_CTX_T_CTRL,
+	UNVME_CTX_T_SQ,
+	UNVME_CTX_T_CQ,
+	UNVME_CTX_T_NS,
+};
+
+struct unvme_ctx {
+	enum unvme_ctx_type type;
+
+	union {
+		struct {
+			uint8_t iosqes;
+			uint8_t iocqes;
+			uint8_t mps;
+			uint8_t css;
+		} ctrl;
+
+		struct {
+			uint32_t qid;
+			uint32_t qsize;
+			uint32_t cqid;
+		} sq;
+
+		struct {
+			uint32_t qid;
+			uint32_t qsize;
+			int vector;
+		} cq;
+
+		struct {
+			uint32_t nsid;
+		} ns;
+	};
+
+	struct list_node list;
+};
+
+/* Reaped CQ */
+struct unvme_rcq {
+	struct nvme_cqe *cqe;
+	int qsize;
+	uint16_t head;
+	uint16_t tail;
+};
+
+struct unvme_cq_reaper {
+	struct unvme *u;
+	int refcnt;
+
+	int vector;
+	int epoll_fd;
+	int efd;
+	pthread_t th;
+	struct unvme_rcq rcq;
 };
 
 /*
