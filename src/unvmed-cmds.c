45aa06c133a06b427128fbf3f261cd88a32102d9
Minwoo Im <minwoo.im@samsung.com>
2025-04-21 14:40:34 +0900

    unvmed: support omitting -s|--size to set-features-hmb
    User can skip giving --size= argument in set-features-hmb command in
    case --disable option is given since descriptors of size will be
    ignored.
    
    Signed-off-by: Minwoo Im <minwoo.im@samsung.com>

--- src/unvmed-cmds.c
+++ src/unvmed-cmds.c
@@ -1389,12 +1389,14 @@ int unvme_set_features_hmb(int argc, char *argv[], struct unvme_msg *msg)
 {
 	const char *desc =
 		"Submit a Set Features admin command to the given <device> NVMe controller\n"
-		"to enable Host Memory Buffer.";
+		"to enable Host Memory Buffer.\n"
+		"\n"
+		"-s|--size argument will be ignored in case --disable is given.";
 
 	struct arg_rex *dev = arg_rex1(NULL, NULL, UNVME_BDF_PATTERN, "<device>", 0, "[M] Device bdf");
 	struct arg_lit *enable = arg_lit0(NULL, "enable", "[O] Enable HMB (EHM=1)");
 	struct arg_lit *disable = arg_lit0(NULL, "disable", "[O] Disable HMB (EHM=0)");
-	struct arg_int *size = arg_intn("s", "size", "<n>", 1, 256, "[M] Number of contiguous memory page size(CC.MPS) for a single HMB descriptor.  Multiple sizes can be given.");
+	struct arg_int *size = arg_intn("s", "size", "<n>", 0, 256, "[M] Number of contiguous memory page size(CC.MPS) for a single HMB descriptor.  Multiple sizes can be given.");
 	struct arg_lit *help = arg_lit0("h", "help", "Show help message");
 	struct arg_end *end = arg_end(UNVME_ARG_MAX_ERROR);
 	void *argtable[] = {dev, enable, disable, size, help, end};
@@ -1413,6 +1415,12 @@ int unvme_set_features_hmb(int argc, char *argv[], struct unvme_msg *msg)
 		goto out;
 	}
 
+	if (arg_boolv(enable) && !arg_boolv(size)) {
+		unvme_pr_err("-s|--size should be given\n");
+		ret = EINVAL;
+		goto out;
+	}
+
 	u = unvmed_get(arg_strv(dev));
 	if (!u) {
 		unvme_pr_err("%s is not added to unvmed\n", arg_strv(dev));
@@ -1427,8 +1435,12 @@ int unvme_set_features_hmb(int argc, char *argv[], struct unvme_msg *msg)
 		goto out;
 	}
 
-	ret = unvmed_set_features_hmb(u, arg_boolv(enable),
-			(uint32_t *)size->ival, size->count, &cqe);
+	if (arg_boolv(enable)) {
+		ret = unvmed_set_features_hmb(u, true, (uint32_t *)size->ival,
+				size->count, &cqe);
+	} else
+		ret = unvmed_set_features_hmb(u, false, NULL, 0, &cqe);
+
 	if (!ret)
 		unvme_pr_err("dw0: %#x\n", le32_to_cpu(cqe.dw0));
 	else if (ret > 0)
