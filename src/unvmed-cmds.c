SamsungDS/unvme-cli

commit a150423815135662dacb6a91250c265096e11ae3

unvmed-cmds: fix to grab usq lock before alloc cmd
Currently, there is a timing issue that allocated and not submitted @cmd
instance is not canceled in reset thread. Because of this, the reset
thread can not be done.

        Thread 1 (passthru)             Thread 2 (reset)
        ------------------              ----------------
        cmd allocation
                                        lock(usq)
                                        cancel cmds (only submitted cmds)
                                        wait to @usq->nr_cmds == 0 (hang)
                                        unlock(usq)
        lock(usq) (hang)
        cmd post

This patch grabs @usq->lock before allocating @cmd instance, so that all
commands are submitted right after allocated.

At now, we apply this only to `unvme_passthru` for testing. After
verification, we have to spread out to other cli commands.

Signed-off-by: Jonghwi Jeong <jongh2.jeong@samsung.com>

src/unvmed-cmds.c
@@ -2675,6 +2675,8 @@ int unvme_passthru(int argc, char *argv[], struct unvme_msg *msg)
 		__cidp = &__cid;
 	}
 
+	unvmed_sq_enter(usq);
+
 	/*
 	 * If --prp1 and --prp2 both are not given, libunvmed will take care of
 	 * the DMA buffers.
@@ -2771,7 +2773,12 @@ int unvme_passthru(int argc, char *argv[], struct unvme_msg *msg)
 	if (arg_boolv(verbose))
 		unvme_pr_sqe(&cmd->sqe);
 
-	ret = unvmed_cmd_issue_and_wait(cmd);
+	cmd->flags |= UNVMED_CMD_F_WAKEUP_ON_CQE;
+	unvmed_cmd_post(cmd, &cmd->sqe, cmd->flags);
+	unvmed_sq_exit(usq);
+
+	unvmed_cmd_wait(cmd);
+	ret = unvmed_cqe_status(&cmd->cqe);
 
 	if ((ret >= 0 && arg_boolv(verbose)) || ret > 0)
 		unvme_pr_cqe(&cmd->cqe);

commit c72e210dd3e374fb4516be354940378da4437186

unvmed/cmds: fix to relase @usq->lock in error
In commit a150423815135662dacb6a91250c265096e11ae3('unvmed-cmds: fix to
grab usq lock before alloc cmd'), we fixed to get @usq->lock before @cmd
instance allocated. However, in some error cases like @usq->cid
allocation failed, we didn't release the @usq->lock.

This patch fixed to release @usq->lock in such cases.

Fixes: a150423815135662dacb6a91250c265096e11ae3

Signed-off-by: Jonghwi Jeong <jongh2.jeong@samsung.com>

src/unvmed-cmds.c
@@ -2686,7 +2686,7 @@ int unvme_passthru(int argc, char *argv[], struct unvme_msg *msg)
 		if (len < 0) {
 			unvme_pr_err("failed to allocate buffer\n");
 			ret = errno;
-			goto out;
+			goto exit;
 		}
 
 		cmd = unvmed_alloc_cmd(u, usq, __cidp, buf, len);
@@ -2695,7 +2695,7 @@ int unvme_passthru(int argc, char *argv[], struct unvme_msg *msg)
 
 			pgunmap(buf, len);
 			ret = errno;
-			goto out;
+			goto exit;
 		}
 
 		if (_write) {
@@ -2706,7 +2706,7 @@ int unvme_passthru(int argc, char *argv[], struct unvme_msg *msg)
 				unvmed_cmd_put(cmd);
 				pgunmap(buf, len);
 				ret = ENOENT;
-				goto out;
+				goto exit;
 			}
 		}
 
@@ -2721,7 +2721,7 @@ int unvme_passthru(int argc, char *argv[], struct unvme_msg *msg)
 			unvme_pr_err("failed to allocate a command instance\n");
 
 			ret = errno;
-			goto out;
+			goto exit;
 		}
 	}
 
@@ -2749,7 +2749,7 @@ int unvme_passthru(int argc, char *argv[], struct unvme_msg *msg)
 		unvmed_cmd_put(cmd);
 		pgunmap(buf, len);
 		ret = 0;
-		goto out;
+		goto exit;
 	}
 
 	if (arg_boolv(nodb))
@@ -2793,6 +2793,8 @@ free:
 	unvmed_cmd_put(cmd);
 	if (buf && len)
 		pgunmap(buf, len);
+exit:
+	unvmed_sq_exit(usq);
 out:
 	unvme_free_args(argtable);
 	return ret;