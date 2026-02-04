SamsungDS/unvme-cli

commit 1de10fae4c64a8ae4349b2f62fc1f0792bef2e4b

unvme-cli: support qaddr options for admin sq/cq
User can simply give ``iova`` for admin sq and cq when creating the
admin sq/cq.  This patch unified the @usq and @ucq management for adminq
just like the other I/O queues.

Signed-off-by: Minwoo Im <minwoo.im@samsung.com>

src/unvmed-cmds.c
@@ -647,7 +647,9 @@ int unvme_create_adminq(int argc, char *argv[], struct unvme_msg *msg)
 {
 	struct unvme *u;
 	struct arg_rex *dev;
+	struct arg_dbl *sqaddr;
 	struct arg_int *sqsize;
+	struct arg_dbl *cqaddr;
 	struct arg_int *cqsize;
 	struct arg_lit *noint;
 	struct arg_lit *help;
@@ -660,13 +662,20 @@ int unvme_create_adminq(int argc, char *argv[], struct unvme_msg *msg)
 
 	void *argtable[] = {
 		dev = arg_rex1(NULL, NULL, UNVME_BDF_PATTERN, "<device>", 0, "[M] Device bdf"),
+		sqaddr = arg_dbl0("S", "sqaddr", "<iova>", "[O] pre-mapped I/O virtual address of Submission queue"),
 		sqsize = arg_int1("s", "sqsize", "<n>", "[M] Submission Queue size (1-based)"),
+		cqaddr = arg_dbl0("C", "cqaddr", "<iova>", "[O] pre-mapped I/O virtual address of Completion queue"),
 		cqsize = arg_int1("c", "cqsize", "<n>", "[M] Completion Queue size (1-based)"),
 		noint = arg_lit0(NULL, "noint", "[O] Skip IRQ initialization"),
 		help = arg_lit0("h", "help", "Show help message"),
 		end = arg_end(UNVME_ARG_MAX_ERROR),
 	};
+	struct unvme_sq *usq;
+	struct unvme_cq *ucq;
 	int ret = 0;
+
+	arg_dblv(sqaddr) = 0;
+	arg_dblv(cqaddr) = 0;
 
 	unvme_parse_args_locked(argc, argv, argtable, help, end, desc);
 
@@ -683,13 +692,48 @@ int unvme_create_adminq(int argc, char *argv[], struct unvme_msg *msg)
 		goto out;
 	}
 
-	if (unvmed_create_adminq(u, arg_intv(sqsize), arg_intv(cqsize),
-				!arg_boolv(noint))) {
-		unvme_pr_err("failed to create adminq\n");
+	if (arg_boolv(cqaddr))
+		ucq = unvmed_init_cq_iova(u, 0 /* qid */, arg_intv(cqsize),
+				0 /* vector */, 1 /* pc */, arg_dblv(cqaddr));
+	else
+		ucq = unvmed_init_cq(u, 0 /* qid */, arg_intv(cqsize),
+				0 /* vector */, 1 /* pc */);
+	if (!ucq) {
+		unvme_pr_err("failed to allocate @ucq instance for adminq\n");
 		ret = errno;
 		goto out;
 	}
 
+	if (arg_boolv(sqaddr))
+		usq = unvmed_init_sq_iova(u, 0 /* qid */, arg_intv(sqsize),
+				0 /* cqid */, 0 /* qprio */, 1 /* pc */,
+				0 /* nvmsetid */, arg_dblv(sqaddr));
+	else
+		usq = unvmed_init_sq(u, 0 /* qid */, arg_intv(sqsize),
+				0 /* cqid */, 0 /* qprio */, 1 /* pc */,
+				0 /* nvmsetid */);
+	if (!usq) {
+		unvme_pr_err("failed to allocate @usq instance for adminq\n");
+		ret = errno;
+		goto ucq;
+	}
+
+	if (unvmed_configure_adminq(u, usq, ucq)) {
+		unvme_pr_err("failed to configure adminq to register\n");
+		ret = errno;
+		goto usq;
+	}
+
+	unvmed_enable_sq(usq);
+	unvmed_enable_cq(ucq);
+
+	unvme_free_args(argtable);
+	return 0;
+
+usq:
+	unvmed_free_sq(u, 0);
+ucq:
+	unvmed_free_cq(u, 0);
 out:
 	unvme_free_args(argtable);
 	return ret;

lib/libunvmed.c
@@ -1578,6 +1578,9 @@ static struct unvme_sq *unvmed_init_usq(struct unvme *u, uint32_t qid,
 	ucq->usq = usq;
 	usq->flags = 0;
 
+	if (!qid)
+		u->ctrl.adminq.sq = usq->q;
+
 	unvmed_sq_exit(usq);
 
 	return usq;
@@ -1657,6 +1660,10 @@ static struct unvme_cq *unvmed_init_ucq(struct unvme *u, uint32_t qid,
 	ucq->qsize = qsize;
 	ucq->vector = vector;
 	ucq->pc = pc;
+
+	if (!qid)
+		u->ctrl.adminq.cq = ucq->q;
+
 	unvmed_cq_exit(ucq);
 
 	return ucq;
@@ -2282,6 +2289,27 @@ static void unvmed_discard_cq(struct unvme *u, uint32_t qid)
 		unvmed_cq_exit(ucq);
 	}
 	pthread_rwlock_unlock(&u->cqs_lock);
+}
+
+int unvmed_configure_adminq(struct unvme *u, struct unvme_sq *usq,
+			    struct unvme_cq *ucq)
+{
+	uint32_t aqa = unvmed_read32(u, NVME_REG_AQA);
+
+	if (!usq || !ucq) {
+		unvmed_log_err("@usq or @ucq is NULL");
+		errno = EINVAL;
+		return -1;
+	}
+
+	aqa = usq->qsize - 1;
+	aqa |= (ucq->qsize - 1) << 16;
+
+	unvmed_write32(u, NVME_REG_AQA, cpu_to_le32(aqa));
+	unvmed_write64(u, NVME_REG_ASQ, cpu_to_le64(usq->q->mem.iova));
+	unvmed_write64(u, NVME_REG_ACQ, cpu_to_le64(ucq->q->mem.iova));
+
+	return 0;
 }
 
 int unvmed_create_adminq(struct unvme *u, uint32_t sq_size,
@@ -4103,12 +4131,6 @@ static struct unvme_cq *__unvmed_init_cq(struct unvme *u, uint32_t qid, uint32_t
 	if (vector >= 0 && unvmed_init_irq(u, vector)) {
 		unvmed_log_err("failed to initialize irq (nr_irqs=%d, vector=%d, errno=%d \"%s\")",
 				u->nr_irqs, vector, errno, strerror(errno));
-		return NULL;
-	}
-
-	if (!u->asq) {
-		unvmed_log_err("failed to initialize I/O CQ due to no Admin SQ");
-		errno = EPERM;
 		return NULL;
 	}
 

lib/libunvmed.h
@@ -922,6 +922,18 @@ void unvmed_reset_ctrl(struct unvme *u);
 void unvmed_reset_ctrl_graceful(struct unvme *u);
 
 /**
+ * unvmed_configure_adminq - Configure admin SQ/CQ to Controller register
+ * @u: &struct unvme
+ * @usq: &struct unvme_sq (can be NULL)
+ * @ucq: &struct unvme_cq (can be NULL)
+ *
+ * @usq and @ucq can be NULL.  If NULL, no ops, but if not, controller register
+ * will be configured.
+ */
+int unvmed_configure_adminq(struct unvme *u, struct unvme_sq *usq,
+			    struct unvme_cq *ucq);
+
+/**
  * unvmed_create_adminq - Configure admin SQ and CQ
  * @u: &struct unvme
  * @sq_size: number of queue entries

commit 40212fe9feb3ec2ec39ab97fb64b718e7d010b55

unvmed/cmds: fix missing --noint in `unvme create-adminq`
This patch fixed Commit 1de10fae4c64 ("unvme-cli: support qaddr options
for admin sq/cq") which missed --noint argument passing to libunvmed to
enable or disable interrupt vector for admin-cq.

Fixes: 1de10fae4c64
Signed-off-by: Minwoo Im <minwoo.im@samsung.com>

src/unvmed-cmds.c
@@ -692,6 +692,7 @@ int unvme_create_adminq(int argc, char *argv[], struct unvme_msg *msg)
 	};
 	struct unvme_sq *usq;
 	struct unvme_cq *ucq;
+	int vector;
 	int ret = 0;
 
 	arg_dblv(sqaddr) = 0;
@@ -712,12 +713,14 @@ int unvme_create_adminq(int argc, char *argv[], struct unvme_msg *msg)
 		goto out;
 	}
 
+	vector = arg_boolv(noint) ? -1 : 0;
+
 	if (arg_boolv(cqaddr))
 		ucq = unvmed_init_cq_iova(u, 0 /* qid */, arg_intv(cqsize),
-				0 /* vector */, 1 /* pc */, arg_dblv(cqaddr));
+				vector, 1 /* pc */, arg_dblv(cqaddr));
 	else
 		ucq = unvmed_init_cq(u, 0 /* qid */, arg_intv(cqsize),
-				0 /* vector */, 1 /* pc */);
+				vector, 1 /* pc */);
 	if (!ucq) {
 		unvme_pr_err("failed to allocate @ucq instance for adminq\n");
 		ret = errno;