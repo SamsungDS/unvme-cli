SamsungDS/unvme-cli

commit 8d08583586181a9705afe6e90922faae259fb209

libunvmed: add @usq->cids to manage command ids
libunvmed has fully relied on libvfn's @rq->cid for simplicity, but user
might want to manipulate @cmd instance with a specific command id.  This
patch introduced atomic command id APIs to manage command ids
thread-safely.  @usq->cids represents the &struct unvme_bitmap to manage
cid within a bitmap.

Signed-off-by: Minwoo Im <minwoo.im@samsung.com>

lib/libunvmed.c
@@ -1081,6 +1081,29 @@ out:
 	return ret;
 }
 
+static int unvmed_cid_init(struct unvme_sq *usq, size_t size)
+{
+	struct unvme_bitmap *bitmap = &usq->cids;
+
+	bitmap->size = size;
+	bitmap->nr_bytes = (size + 8 - 1) / 8;
+	bitmap->bits = calloc(bitmap->nr_bytes, sizeof(uint8_t));
+
+	if (!bitmap->bits) {
+		errno = ENOMEM;
+		return -1;
+	}
+
+	return 0;
+}
+
+static void unvmed_cid_free(struct unvme_sq *usq)
+{
+	struct unvme_bitmap *bitmap = &usq->cids;
+
+	free(bitmap->bits);
+}
+
 static struct unvme_sq *unvmed_init_usq(struct unvme *u, uint32_t qid,
 					uint32_t qsize, struct unvme_cq *ucq)
 {
@@ -1109,13 +1132,18 @@ static struct unvme_sq *unvmed_init_usq(struct unvme *u, uint32_t qid,
 	if (alloc) {
 		usq->cmds = calloc(qsize, sizeof(struct unvme_cmd));
 
+		if (unvmed_cid_init(__to_sq(usq), qsize) < 0) {
+			free(usq->cmds);
+			free(usq);
+			return NULL;
+		}
+
 		pthread_rwlock_wrlock(&u->sq_list_lock);
 		list_add_tail(&u->sq_list, &usq->list);
 		u->sqs[qid] = __to_sq(usq);
 		if (!qid)
 			u->asq = __to_sq(usq);
 		pthread_rwlock_unlock(&u->sq_list_lock);
-
 		unvmed_sq_get(u, qid);
 	}
 
@@ -1129,6 +1157,7 @@ void unvmed_enable_sq(struct unvme_sq *usq)
 
 static void __unvmed_free_usq(struct unvme *u, struct __unvme_sq *usq)
 {
+	unvmed_cid_free(__to_sq(usq));
 	u->sqs[unvmed_sq_id(usq)] = NULL;
 	if (!unvmed_sq_id(usq))
 		u->asq = NULL;

lib/libunvmed-cmds.c
@@ -8,6 +8,118 @@
 #include "libunvmed.h"
 #include "libunvmed-private.h"
 
+static inline int __find_first_zero(uint8_t byte)
+{
+	if (byte == -1)
+		return -1;
+
+	for (int i = 0; i < 8; i++) {
+		if (!(byte & (1 << i)))
+			return i;
+	}
+
+	return -1;
+}
+
+static int unvmed_cid_alloc(struct unvme_sq *usq, uint16_t *cid)
+{
+	struct unvme_bitmap *bitmap = &usq->cids;
+	int i = atomic_load_acquire(&bitmap->top);
+
+	for (;;) {
+		uint8_t old = atomic_load_acquire(&bitmap->bits[i]);
+		uint8_t desired;
+		int pos;
+
+		if (old == 0xFF)
+			break;
+
+		pos = __find_first_zero(old);
+		if (pos < 0)
+			break;
+
+		desired = old | (1 << pos);
+		if (atomic_cmpxchg(&bitmap->bits[i], old, desired)) {
+			*cid = i * 8 + pos;
+			return 0;
+		}
+	}
+
+	for (i = 0; i < bitmap->nr_bytes; ++i) {
+		uint8_t old = atomic_load_acquire(&bitmap->bits[i]);
+		uint8_t desired;
+		int pos;
+
+		if (old == 0xFF)
+			continue;
+
+		pos = __find_first_zero(old);
+		if (pos < 0)
+			continue;
+
+		desired = old | (1 << pos);
+		if (atomic_cmpxchg(&bitmap->bits[i], old, desired)) {
+			atomic_store_release(&bitmap->top, i);
+			*cid = i * 8 + pos;
+			return 0;
+		}
+	}
+
+	errno = EBUSY;
+	return -1;
+}
+
+static int unvmed_cid_alloc_n(struct unvme_sq *usq, uint16_t cid)
+{
+	struct unvme_bitmap *bitmap = &usq->cids;
+	size_t byte_index = cid / 8;
+	size_t bit_index = cid % 8;
+
+	uint8_t mask = 1 << bit_index;
+	uint8_t old;
+	uint8_t desired;
+
+	if (cid >= bitmap->size) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	do {
+		old = atomic_load_acquire(&bitmap->bits[byte_index]);
+		if (old & mask) {
+			errno = EBUSY;
+			return -1;
+		}
+
+		desired = old | mask;
+	} while (!atomic_cmpxchg(&bitmap->bits[byte_index], old, desired));
+
+	return 0;
+}
+
+static int unvmed_cid_free(struct unvme_sq *usq, uint16_t cid)
+{
+	struct unvme_bitmap *bitmap = &usq->cids;
+	size_t byte_index = cid / 8;
+	size_t bit_index = cid % 8;
+
+	uint8_t mask = ~(1 << bit_index);
+	uint8_t old;
+	uint8_t desired;
+
+	if (cid >= bitmap->size) {
+		errno = EINVAL;
+		return -1;
+	}
+
+	do {
+		old = atomic_load_acquire(&bitmap->bits[byte_index]);
+		desired = old & mask;
+	} while (!atomic_cmpxchg(&bitmap->bits[byte_index], old, desired));
+
+	return 0;
+}
+
 /*
  * This function is from `libnvme` with few modifications.  It updates the
  * given @sqe with cdw2, cdw3, cdw14 and cdw15, remaining fields should be
@@ -85,15 +197,24 @@ static struct unvme_cmd *__unvmed_cmd_alloc(struct unvme *u, struct unvme_sq *us
 {
 	struct unvme_cmd *cmd;
 	struct nvme_rq *rq;
+	uint16_t cid;
 
 	rq = nvme_rq_acquire_atomic(usq->q);
 	if (!rq)
 		return NULL;
 
+	cid = rq->cid;
+
+	if (unvmed_cid_alloc_n(usq, cid)) {
+		nvme_rq_release_atomic(rq);
+		return NULL;
+	}
+
 	atomic_inc(&usq->nr_cmds);
 
-	cmd = &usq->cmds[rq->cid];
+	cmd = &usq->cmds[cid];
 	memset(cmd, 0, sizeof(*cmd));
+	cmd->cid = cid;
 	cmd->u = u;
 	cmd->usq = usq;
 	cmd->rq = rq;
@@ -182,10 +303,12 @@ void __unvmed_cmd_free(struct unvme_cmd *cmd)
 {
 	struct unvme_sq *usq = cmd->usq;
 	struct nvme_rq *rq = cmd->rq;
+	uint16_t cid = cmd->cid;
 
 	memset(cmd, 0, sizeof(*cmd));
 	atomic_dec(&usq->nr_cmds);
 
+	unvmed_cid_free(usq, cid);
 	nvme_rq_release_atomic(rq);
 }
 

lib/libunvmed.h
@@ -74,6 +74,19 @@ struct nvme_cq;
 struct nvme_rq;
 
 /*
+ * struct unvme_bitmap - atomic bitmap data structure
+ * @size: Number of bits
+ * @nr_bytes: Number of bytes to cover @size
+ * @bits: Actual bitmap data array
+ */
+struct unvme_bitmap {
+	size_t size;
+	size_t nr_bytes;
+	uint8_t *bits;
+	int top;
+};
+
+/*
  * struct unvme_ns - Namespace instance
  * @u: unvme controller instance
  * @refcnt: reference count
@@ -125,6 +138,7 @@ struct name {			\
 	struct nvme_sq *q;	\
 	struct unvme_cq *ucq;	\
 	struct unvme_cmd *cmds; \
+	struct unvme_bitmap cids;\
 	int nr_cmds;		\
 	pthread_spinlock_t lock;\
 	bool enabled;		\
@@ -212,6 +226,7 @@ struct unvme_cmd {
 	unsigned long flags;
 
 	struct unvme_sq *usq;
+	uint16_t cid;
 
 	/*
 	 * rq->opaque will point to the current structure pointer.

commit a31229f00920d1fa3f1ef8e877da9702f3de152a

libunvmed: fix 8bytes unaligned qsize cid management
In 8bytes-unaligned size(e.g., qsize=35), the trailing bits are still
representing *empty* bits leading libunvmed treats as remaining bits.
Fix it by pre-marking the un-used bits at the initialization step.

Fixes: 8d0858358618 ("libunvmed: add @usq->cids to manage command ids")
Signed-off-by: Minwoo Im <minwoo.im@samsung.com>

lib/libunvmed.c
@@ -1243,6 +1243,12 @@ static int unvmed_cid_init(struct unvme_sq *usq, size_t size)
 		return -1;
 	}
 
+	/*
+	 * Mark un-used trailing bits in-used.
+	 */
+	if (size % 8)
+		bitmap->bits[bitmap->nr_bytes - 1] = (uint8_t)(0xff << (size % 8));
+
 	return 0;
 }
 